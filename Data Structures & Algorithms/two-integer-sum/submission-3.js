0a6fe3011d342a8a8f2e2eb234f0637da0d02eb5
Alok Raj <alokr417@gmail.com>
2026-04-28 23:53:37 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    // HashMap
+    twoSum(nums, target) {
+        const obj = {};
+
+        for (let i = 0; i < nums.length; i++) {
+            obj[nums[i]] = i
+        }
+        for (let i = 0; i < nums.length; i++) {
+            let diff = target - nums[i];
+
+            // if diff is found in object and not equal to current index
+            if (obj[diff] !== undefined && obj[diff] !== i) {
+                return [i, obj[diff]]
+            }
+        }
+
+        return [];
+        
+    }
+}
